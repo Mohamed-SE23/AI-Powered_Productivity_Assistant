cd57b355e3c0dcddf8a1c19747242c424fdbd80b
MohamedAlamen123 <alamenmohamed536@gmail.com>
2025-01-20 10:16:48 +0200

    edit redis file to check redis connectivity

--- server/src/config/redis.js
+++ server/src/config/redis.js
@@ -1,7 +1,21 @@
 import { createClient } from 'redis';
 
 const redisClient = createClient({
-  url: process.env.REDIS_URL,
+  url: process.env.REDIS_URL, // Use environment variable for Redis URL
+  socket: {
+    tls: false, // Set to true if using a secure connection
+  },
 });
+
+redisClient.on('error', (err) => console.error('Redis client error:', err));
+
+(async () => {
+  try {
+    await redisClient.connect();
+    console.log('Connected to Redis');
+  } catch (error) {
+    console.error('Error connecting to Redis:', error);
+  }
+})();
 
 export default redisClient;
