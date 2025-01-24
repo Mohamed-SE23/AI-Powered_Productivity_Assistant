fb4531d7cd9a47e73efd40a12e57cbaf1539ca64
MohamedAlamen123 <alamenmohamed536@gmail.com>
2025-01-24 07:59:07 +0200

    fix url backend end point

--- client/src/app/tasksSlice.js
+++ client/src/app/tasksSlice.js
@@ -2,9 +2,7 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 import toast from 'react-hot-toast';
 
-const serverUrl = process.env.NODE_ENV === 'production'
-  ? 'https://ai-powered-productivity-assistant.onrender.com/api/v1'
-  : '/api/v1'; // Proxy only used during development
+const serverUrl = process.env.NODE_ENV === 'https://ai-powered-productivity-assistant.onrender.com/api/v1'; // Proxy only used during development
 
 // get the token 
 const user = JSON.parse(localStorage.getItem('user'));
