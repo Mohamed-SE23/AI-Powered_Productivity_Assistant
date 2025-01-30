2b837f566b8f79b1afb5ccf624e482cd44570bdc
MohamedAlamen123 <alamenmohamed536@gmail.com>
2025-01-30 05:50:42 +0200

    use mailslurp to send emails

--- server/src/config/emailConfig.js
+++ server/src/config/emailConfig.js
@@ -1,5 +1,8 @@
 export default {
     service: "Gmail",
+    host: 'mx.mailslurp.com',
+    port: '2525',
+    secure: false,
     auth: {
       user: process.env.EMAIL_USER, // Add your email in .env
       pass: process.env.EMAIL_PASS, // Add your email password in .env
