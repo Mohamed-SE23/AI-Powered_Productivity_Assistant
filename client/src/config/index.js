fc473e59af73ebffbdcaa31553afdcdedff7df81
ABDALLA MOHAMED <proabdallami@gmail.com>
2025-07-23 13:06:57 +0300

    Update development server

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== "production";
 
-export const server = dev ? "http://localhost:5000" : "https://proabdallami-back.serveo.net";
+export const server = dev ? "http://localhost:5000" : "https://verified-deer-oriented.ngrok-free.app";
